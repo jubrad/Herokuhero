aef3baf866e5700cf6762fbaf3383c345b7c62f7
jubrad <bradfield.justin7@gmail.com>
2013-12-05 14:20:49 -0500

    changed port to look for env for heroku

--- app.js
+++ app.js
@@ -54,7 +54,9 @@ MongoClient.connect('mongodb://localhost:27017/ipdb', function (error, db) {
     /*
       server
     */
-    app.listen(3000, function(){
+  var port = process.env.PORT || 3000;
+
+    app.listen(port, function(){
           console.log("Express server listening on port %d in %s mode", 3000, app.settings.env);
     });
 
